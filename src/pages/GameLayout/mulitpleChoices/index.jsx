2b8bce3dd882774f42d06f92c5c67cf4e4676f15
detectiveseo <detectiveseo1@gmai.com>
2023-09-18 17:58:07 +0600

    json file change and changed layout

--- src/pages/GameLayout/mulitpleChoices/index.jsx
+++ src/pages/GameLayout/mulitpleChoices/index.jsx
@@ -2,45 +2,38 @@ import { useEffect, useState } from "react";
 import uesData from "../../../hooks/uesData";
 
 const MulitpleChoices = () => {
-
     const [data, setData] = useState([]);
+    const [currentIndex, setCurrentIndex] = useState(0);
 
     useEffect(() => {
-        uesData().then(responce => setData(responce));
+        uesData().then(responce => setData(responce.questions));
     }, [])
 
-    const items = data?.questions?.item;
 
-    const filterItems = items?.filter(item => item.category === "Multiple Choices")
 
     return (
-        <div>
-            {
-                filterItems?.slice(1, 10).map((datas, index) => {
-                    const question = datas.landscape.question.__cdata;
-                    const options = datas?.landscape?.answers?.answer;
+        <div className="text-white text-8xl">
+            <h1 className="font-bold">
+                {data[currentIndex]?.question}
+            </h1>
 
-                    return (
-                        <div
-                            key={index}
-                            className="text-white text-8xl">
-                            <h1 className="font-bold">{question}</h1>
-
-                            <div className="flex justify-center flex-wrap items-center gap-20 mt-10">
-                                {
-                                    options?.map((option, index) => {
-                                        return (
-                                            <div key={index}>
-                                                <button className="btn bg-[#A2CD4A] w-96 py-5 rounded-lg shadow-solid text-5xl" >{option.__cdata}</button>
-                                            </div>
-                                        )
-                                    })
-                                }
+            <div className="flex justify-center flex-wrap items-center gap-10 mt-10">
+                {
+                    data[currentIndex]?.options?.map((option, index) => {
+                        return (
+                            <div key={index}>
+                                <button className="btn bg-[#A2CD4A] w-96 py-5 rounded-lg shadow-solid text-5xl" >{option}</button>
                             </div>
-                        </div>
-                    )
-                })
-            }
+                        )
+                    })
+                }
+            </div>
+
+            <div>
+                <button 
+                onClick={() => setCurrentIndex((prev) =>  prev + 1)}
+                className="btn bg-[#A2CD4A] w-96 py-5 rounded-lg shadow-solid text-5xl mt-20">Done</button>
+            </div>
         </div>
     );
 };
